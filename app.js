57659579dee4f21a7ad0ef3cc29dedbfad92c7c3
Furqan45 <36231004+Furqan45@users.noreply.github.com>
2023-03-31 23:22:48 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,64 @@
+// ------------------------------------xxxxxxxxxxxxxxxxxxxxxxxxx---------------------------
+// alert("Amm khaya ga");
+
+// ------------------------------------xxxxxxxxxxxxxxxxxxxxxxxxx---------------------------
+
+// console.log("ramazan");
+// ------------------------------------xxxxxxxxxxxxxxxxxxxxxxxxx---------------------------
+
+// var title = "Ramzan Transmition";
+
+// console.log(title);
+
+// ------------------------------------xxxxxxxxxxxxxxxxxxxxxxxxx---------------------------
+// var x = 10;
+// var y = x + 20
+
+// console.log(y);
+
+// ------------------------------------xxxxxxxxxxxxxxxxxxxxxxxxx---------------------------
+
+// var a = 10;
+// var b = 20;
+// var c = 30;
+
+// var d = (a+b)-c;
+
+// console.log(d);
+
+// ------------------------------------xxxxxxxxxxxxxxxxxxxxxxxxx---------------------------
+
+// var a = "10";
+// var b = 20
+
+// var c = b-a;
+// console.log(c)
+
+// ------------------------------------xxxxxxxxxxxxxxxxxxxxxxxxx---------------------------
+
+// var firstname = prompt();
+// console.log(firstname);
+
+
+// var firstname = prompt ("Enter First Name");
+// var lastname = prompt("Enter last Name");
+// var city = prompt ("Enter city", "Karachi");
+
+// console.log(firstname + "" + lastname + " From " + city);
+
+
+// ------------------------------------xxxxxxxxxxxxxxxxxxxxxxxxx---------------------------
+
+// var mithaiMilRahihai = true;
+
+// if(mithaiMilRahihai == true){console.log("ajao");
+// }
+
+// else{
+//     console.log("Dusri Masjid jao");
+// }
+
+
+
+
+
